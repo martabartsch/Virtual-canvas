57eeaef4bc913c16062267d646cb2d1f6dd80c5a
martaa2b <martabartsch@outlook.com>
2023-11-14 18:37:57 +0100

    A basic login and registration panel has been added. Added docker-compose mongodb and mongo-express build service available

---
+++ backend/app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request, redirect, url_for
+from user.database import UserDatabase
+from dotenv import load_dotenv
+import os
+
+app = Flask(__name__, template_folder='../frontend/templates', static_folder='../frontend/static')
+
+load_dotenv()
+app.secret_key = os.getenv('secret_key')
+
+user_db = UserDatabase()
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_user():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        result = user_db.login_user(email, password)
+        if result is False:
+            return render_template('login.html', error='Email or password is incorrect')
+        else:
+            return render_template('home.html')
+    return render_template('login.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_user():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        result = user_db.register_user(email, password)
+        print(result)
+        if result == 'ERROR':
+            return render_template('register.html', error='Email already exists')
+        else:
+            return redirect(url_for('login_user'))
+
+    return render_template('register.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0', port=8085)
---
+++ backend/user/database.py
@@ -0,0 +1,53 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from flask import session
+import os
+
+dotenv_path = '/Users/marta/virtual-canvas/.env'
+load_dotenv(dotenv_path)
+
+
+class UserDatabase:
+
+    def __init__(self):
+        # self.mongo_username = os.getenv('MONGO_LOGIN')
+        # self.mongo_password = os.getenv('MONGO_PASSWORD')
+        # self.url = f'mongodb://{self.mongo_username}:{self.mongo_password}@localhost:27017/'
+        self.url = "mongodb://localhost:27017"
+        self.client = MongoClient(self.url)
+        self.db = self.client['users_database']
+        self.collection = self.db['users']
+
+    def get_max_user_id(self):
+        max_id = self.collection.find_one(sort=[('_id', -1)])
+        if max_id:
+            return max_id['_id']
+        else:
+            return 0
+
+    def register_user(self, email, password):
+        max_id = self.get_max_user_id()
+        print(type(max_id))
+        id_user = int(max_id) + 1
+
+        user_exist = self.collection.find_one({"email": email})
+        if user_exist:
+            return 'ERROR'
+        else:
+            user_data = {
+                '_id': str(id_user),
+                'email': email,
+                'password': password,
+            }
+
+        result = self.collection.insert_one(user_data)
+        return result.inserted_id
+
+    def login_user(self, email, password):
+        user_exist = self.collection.find_one({"email": email, "password": password})
+        if user_exist:
+            session['user_id'] = str(user_exist['_id'])
+            session['email'] = user_exist['email']
+            return True
+        else:
+            return False
